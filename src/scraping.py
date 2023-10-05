b66775ab7d8efd0a8a80524c65a0e32f9a419d56
Diogo Costa <diogocosta876@hotmail.com>
2023-10-05 22:46:55 +0100

    error handling and terminal output

--- src/scraping.py
+++ src/scraping.py
@@ -20,48 +20,59 @@ os.makedirs(download_dir, exist_ok=True)
 driver = webdriver.Chrome()
 driver.set_window_size(1200, 1000)
 
+downloaded_count = 0
+error_count = 0
+
 for index, row in df.iterrows():
-    medication_name = row["Product name"]
-    print(f"fetching {medication_name}")
+    try:
+        medication_name = row["Product name"]
 
-    driver.get("https://extranet.infarmed.pt/INFOMED-fo/pesquisa-avancada.xhtml")
+        driver.get("https://extranet.infarmed.pt/INFOMED-fo/pesquisa-avancada.xhtml")
 
-    input_element = driver.find_element(By.ID, "mainForm:medicamento_input")
-    driver.execute_script(f"arguments[0].value = '{medication_name}';", input_element)
+        input_element = driver.find_element(By.ID, "mainForm:medicamento_input")
+        driver.execute_script(
+            f"arguments[0].value = '{medication_name}';", input_element
+        )
 
-    driver.execute_script("window.scrollTo(0, 1000)")
-    search_btn = driver.find_element(By.ID, "mainForm:btnDoSearch").click()
+        driver.execute_script("window.scrollTo(0, 1000)")
+        search_btn = driver.find_element(By.ID, "mainForm:btnDoSearch").click()
 
-    time.sleep(2)
-    bula = driver.find_element(
-        By.ID, "mainForm:dt-medicamentos:0:pesqAvancadaDatableFiIcon"
-    )
-    bula.click()
+        time.sleep(2)
+        bula = driver.find_element(
+            By.ID, "mainForm:dt-medicamentos:0:pesqAvancadaDatableFiIcon"
+        )
+        bula.click()
 
-    pdf_url = driver.current_url
+        pdf_url = driver.current_url
 
-    # New tabs will be the last object in window_handles
-    driver.switch_to.window(driver.window_handles[-1])
+        # New tabs will be the last object in window_handles
+        driver.switch_to.window(driver.window_handles[-1])
 
-    # close the tab
-    driver.close()
+        # close the tab
+        driver.close()
 
-    # switch to the main window
-    driver.switch_to.window(driver.window_handles[0])
+        # switch to the main window
+        driver.switch_to.window(driver.window_handles[0])
 
-    # Download the PDF using requests
-    response = requests.get(pdf_url)
+        # Download the PDF using requests
+        response = requests.get(pdf_url)
 
-    if response.status_code == 200:
-        # Assume the downloaded file is the only file
-        file_name = f"{medication_name}.pdf"
-        file_path = os.path.join(download_dir, file_name)
+        if response.status_code == 200:
+            # Assume the downloaded file is the only file
+            file_name = f"{medication_name}.pdf"
+            file_path = os.path.join(download_dir, file_name)
 
-        with open(file_path, "wb") as pdf_file:
-            pdf_file.write(response.content)
+            with open(file_path, "wb") as pdf_file:
+                pdf_file.write(response.content)
+            downloaded_count += 1
+            print(f"Successfull Downloads: {downloaded_count}")
+        else:
+            raise Exception("Failed to download the PDF")
 
-        print(f"Downloaded: {file_name}")
-    else:
-        print(f"Failed to download: {medication_name}")
+    except Exception as e:
+        error_count += 1
+        print(f"Failed to fetch: {medication_name}  Error Count: {error_count}")
+        # print(e)
+
 
 print("Finished scraping.")
