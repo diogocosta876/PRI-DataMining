fe005370aa50939bc6e33ea37c0c525ca3e31bab
Miguel Teixeira <themutedguy@gmail.com>
2023-11-15 21:53:26 +0000

    initial evaluation code

--- src/evaluation.py
+++ src/evaluation.py
@@ -61,4 +61,49 @@ df = pd.DataFrame([['Metric','Value']] +
 
 with open('results.tex','w') as tf:
     tf.write(df.to_latex())
+    
+    
+    
+    
+    
+    
+    
+    
+# PRECISION-RECALL CURVE
+# Calculate precision and recall values as we move down the ranked list
+precision_values = [
+    len([
+        doc 
+        for doc in results[:idx]
+        if doc['id'] in relevant
+    ]) / idx 
+    for idx, _ in enumerate(results, start=1)
+]
+
+recall_values = [
+    len([
+        doc for doc in results[:idx]
+        if doc['id'] in relevant
+    ]) / len(relevant)
+    for idx, _ in enumerate(results, start=1)
+]
+
+precision_recall_match = {k: v for k,v in zip(recall_values, precision_values)}
+
+# Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
+recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
+recall_values = sorted(set(recall_values))
+
+# Extend matching dict to include these new intermediate steps
+for idx, step in enumerate(recall_values):
+    if step not in precision_recall_match:
+        if recall_values[idx-1] in precision_recall_match:
+            precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
+        else:
+            precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
+
+disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
+disp.plot()
+plt.savefig('precision_recall.pdf')
+
 
