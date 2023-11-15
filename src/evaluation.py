2c2298c23ad1e9e2693e4dc44a94acb87bc23a42
Miguel Teixeira <themutedguy@gmail.com>
2023-11-15 21:53:05 +0000

    initial evaluation code

---
+++ src/evaluation.py
@@ -0,0 +1,64 @@
+# SETUP
+import matplotlib.pyplot as plt
+from sklearn.metrics import PrecisionRecallDisplay
+import numpy as np
+import json
+import requests
+import pandas as pd
+
+QRELS_FILE = ...
+QUERY_URL = ...
+
+# Read qrels to extract relevant documents
+relevant = list(map(lambda el: el.strip(), open(QRELS_FILE).readlines()))
+# Get query results from Solr instance
+results = requests.get(QUERY_URL).json()['response']['docs']
+
+
+# METRICS TABLE
+# Define custom decorator to automatically calculate metric based on key
+metrics = {}
+metric = lambda f: metrics.setdefault(f.__name__, f)
+
+@metric
+def ap(results, relevant):
+    """Average Precision"""
+    precision_values = []
+    relevant_count = 0
+
+    for idx, doc in enumerate(results):
+        if doc['id'] in relevant:
+            relevant_count += 1
+            precision_at_k = relevant_count / (idx + 1)
+            precision_values.append(precision_at_k)
+
+    if not precision_values:
+        return 0.0
+
+    return sum(precision_values)/len(precision_values)
+
+@metric
+def p10(results, relevant, n=10):
+    """Precision at N"""
+    return len([doc for doc in results[:n] if doc['id'] in relevant])/n
+
+def calculate_metric(key, results, relevant):
+    return metrics[key](results, relevant)
+
+# Define metrics to be calculated
+evaluation_metrics = {
+    'ap': 'Average Precision',
+    'p10': 'Precision at 10 (P@10)'
+}
+
+# Calculate all metrics and export results as LaTeX table
+df = pd.DataFrame([['Metric','Value']] +
+    [
+        [evaluation_metrics[m], calculate_metric(m, results, relevant)]
+        for m in evaluation_metrics
+    ]
+)
+
+with open('results.tex','w') as tf:
+    tf.write(df.to_latex())
+
