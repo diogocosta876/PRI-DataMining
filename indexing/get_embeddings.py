66f31780c4b161356403a343e301f0f6cb2afcf6
Manuel Amorim <manueljoaoamorim@gmail.com>
2023-12-12 17:39:22 +0000

    Nova versao semantics

--- indexing/get_embeddings.py
+++ indexing/get_embeddings.py
@@ -9,6 +9,12 @@ def get_embedding(text):
     # The model.encode() method already returns a list of floats
     return model.encode(text, convert_to_tensor=False).tolist()
 
+def transform_none(value):
+    if value is None:
+        return ""
+    else:
+        return value
+
 if __name__ == "__main__":
     # Read JSON from STDIN
     data = json.load(sys.stdin)
@@ -16,13 +22,13 @@ if __name__ == "__main__":
     # Update each document in the JSON data
     for document in data:
         # Extract fields if they exist, otherwise default to empty strings
-        Antes_de_utilizar = document.get("Antes_de_utilizar", "")
-        O_que_e_e_para_que_e_utilizado = document.get("O_que_e_e_para_que_e_utilizado", "")
-        Vias_de_Administracao = document.get("Vias_de_Administracao", "")
-        Duracao_do_Tratamento = document.get("Duracao_do_Tratamento", "")
-        Generico = document.get("Generico", "")
-        Product_name = document.get("Product_name", "")
-        Substancia_Ativa_DCI = document.get("Substancia_Ativa_DCI", "")
+        Antes_de_utilizar = transform_none(document.get("Antes_de_utilizar", ""))
+        O_que_e_e_para_que_e_utilizado = transform_none(document.get("O_que_e_e_para_que_e_utilizado", ""))
+        Vias_de_Administracao = transform_none(document.get("Vias_de_Administracao", ""))
+        Duracao_do_Tratamento = transform_none(document.get("Duracao_do_Tratamento", ""))
+        Generico = transform_none(document.get("Generico", ""))
+        Product_name = transform_none(document.get("Product_name", ""))
+        Substancia_Ativa_DCI = transform_none(document.get("Substancia_Ativa_DCI", ""))
 
         combined_text = Antes_de_utilizar + " " + O_que_e_e_para_que_e_utilizado + " " + Vias_de_Administracao + " " + Duracao_do_Tratamento + " " + Generico + " " + Product_name + " " + Substancia_Ativa_DCI
         document["vector"] = get_embedding(combined_text)
